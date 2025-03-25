c9e7fa5eb8dc1662305508957783d077af295428
Komi W. KOUDO <komi.koudo.qex@eulisa.europa.eu>
2025-03-25 12:57:30 +0100

    fix: update the start bash script

--- client-distribution/src/main/resources/standalone/bash/start.sh
+++ client-distribution/src/main/resources/standalone/bash/start.sh
@@ -4,5 +4,5 @@
 # This scripts assumes that java executable is on the PATH variable!
 #
 
-java -Dloader.path=./lib -cp ./bin/client-starter.jar -Dspring.config.name=connector-client -Dspring.config.location=config/connector-client.properties org.springframework.boot.loader.launch.PropertiesLauncher
+java -Dloader.path=./lib -cp ./bin/client-app-starter.jar -Dspring.config.name=connector-client -Dspring.config.location=config/connector-client.properties org.springframework.boot.loader.launch.PropertiesLauncher
 
