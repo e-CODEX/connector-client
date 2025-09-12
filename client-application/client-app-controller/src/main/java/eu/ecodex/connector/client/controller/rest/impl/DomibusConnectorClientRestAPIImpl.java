e82467b1858a1326179d15b1d22180faeb1f0808
Komi Wolanyo K0UDO <164168359+komi-eulisa@users.noreply.github.com>
2025-09-12 11:49:54 +0200

    build(deps): fix vulnerabilities (#124)

--- client-application/client-app-controller/src/main/java/eu/ecodex/connector/client/controller/rest/impl/DomibusConnectorClientRestAPIImpl.java
+++ client-application/client-app-controller/src/main/java/eu/ecodex/connector/client/controller/rest/impl/DomibusConnectorClientRestAPIImpl.java
@@ -33,7 +33,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
